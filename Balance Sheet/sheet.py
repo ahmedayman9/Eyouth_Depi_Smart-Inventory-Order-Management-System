d8ffd4e486cdcce56a51d0ab08dcae30ff9bcd70
Abdelrahman <abdelrahmanyr05@gmail.com>
2025-08-09 11:53:57 +0300

    Update sheet

--- Balance Sheet/sheet.py
+++ Balance Sheet/sheet.py
@@ -1,1 +1,12 @@
+class SheetManager:
+    def __init__(self, monthly_sheet):
+        self.monthly_sheet = monthly_sheet
 
+    def show_monthly_data(self):
+        for month in range(1, 13):
+            if month in self.monthly_sheet.data:
+                data = self.monthly_sheet.data[month]
+                print(f"Month: {month}, Gross Profit: {data.gross_profit}, Net Profit: {data.net_profit}")
+            else:
+                print(f"Month: {month}, No data available")
+        
