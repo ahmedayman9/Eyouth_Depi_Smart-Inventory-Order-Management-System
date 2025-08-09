d90968b976f051aa0d30c3c911024d2a644fdd36
MohamedAtta2005 <matta5393@gmail.com>
2025-08-09 14:06:59 +0300

    Update balance_sheet.py

--- balance_sheet.py
+++ balance_sheet.py
@@ -1,21 +1,21 @@
-
-class Balance_Sheet():
-    def __init__(self, item_dict):
-        self.items = item_dict 
+class Balance_Sheet(inventory):
+    def __init__(self):
+        super().__init__()
         self.expenses = 0
         self.monthly_sales = {month: {} for month in range(1, 9)}
 
-    def set_expenses(self, amount):
-        self.expenses = amount
+    def set_expenses(self):
+        sum2 = inventory.buy_product(self)
+        self.expenses = sum2
     
-    def record_sale(self, month, item_id, quantity):
+    def record_sale(self, month, item_id ):
         if month not in self.monthly_sales:
             print("Invalid month. Select from 1 to 8.")
             return
         if item_id not in self.items:
             print("Invalid item.")
             return
-        self.monthly_sales[month][item_id] = self.monthly_sales[month].get(item_id, 0) + quantity
+        self.monthly_sales[month][item_id] = self.monthly_sales[month].get(item_id, 0) + self.c
 
     def calculate_monthly_profit(self, month):
         total_revenue = 0
@@ -55,26 +55,3 @@ class Balance_Sheet():
         print(f"total net profit:   ${net_total:.2f}")
         print("=" * 50)
 
-
-items_dict = {
-    201: ["milk", 25, 100],
-    202: ["bread", 20, 80],
-    203: ["eggs", 10, 200],
-    204: ["butter", 50, 60],
-}
-
-
-bs = Balance_Sheet(items_dict)
-
-
-bs.set_expenses(500)
-
-bs.record_sale(1, 201, 50)  # Jan: 50 milk
-bs.record_sale(1, 202, 40)  # Jan: 40 bread
-bs.record_sale(2, 203, 100) # Feb: 100 eggs
-bs.record_sale(8, 204, 20)  # Aug: 20 butter
-
-bs.display_report()
-        
-
-   
