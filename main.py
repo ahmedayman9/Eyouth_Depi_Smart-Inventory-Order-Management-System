78dd6589d759065e56a193b106926632787e19d7
iislamgom3a <iislamgom3a@gmail.com>
2025-08-10 16:06:10 +0300

    added : main logic for login & sign up

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+from modules import balance, customer, inventory, cli_colors
+
+# CLI Colors
+GREEN = cli_colors.COLORS.GREEN
+RED = cli_colors.COLORS.RED
+RESET = cli_colors.COLORS.RESET
 
 
-
+line_sep = str(f"{GREEN}-{RESET}" * 40)
 
 if __name__ == "__main__":
-    pass 
+    print(line_sep)
+    print(f"{GREEN}Welcome to our DEPI Shop!{RESET}")
+    print(line_sep)
+    print(f"{RED}1.login.\n2.sign up.{RESET}")
+    log_choice = int(input("Enter your choice: "))
+    print(line_sep)
+
+    curr_customer = customer.Customer()
+    if log_choice == 1:
+        curr_customer.login()
+    elif log_choice == 2:
+        new_id = curr_customer.sign_up()
+        print(f"Your Id is : {new_id}")
+
+    while True:
+        pass
+        # todo
