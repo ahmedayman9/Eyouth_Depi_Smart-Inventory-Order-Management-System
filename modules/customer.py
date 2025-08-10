b50729c82de32ce9266065d93a581aef0fba4c7e
iislamgom3a <iislamgom3a@gmail.com>
2025-08-10 13:43:44 +0300

    fixed: login, updated file-path

--- modules/customer.py
+++ modules/customer.py
@@ -1,55 +1,50 @@
-import csv 
-import os 
+import csv
+import os
 
-file_name = r'C:\Users\Debi\Desktop\Eyouth_Depi_Smart-Inventory-Order-Management-System\data\customers.csv'
-
-    
+file_name = r"./data/customers.csv"
 
 
-class Customer: 
-    # def __init__(self):
-    row_count = None
+class Customer:
     curr_id = None
-    
+
     def login(self, id, password):
         global curr_id
-        global row_count
-        with open(file_name, 'r', newline='') as file:
+        with open(file_name, "r", newline="") as file:
             reader = csv.reader(file)
-            # rows = list(reader)   
-            # row_count = len(rows) 
+
             next(reader)
-            for row in reader: 
-                print(row)
-                if row[0] == id: 
-                    cnt = 0 
-                    while True: 
-                        if cnt == 3: 
-                            print("You've exceded the number of tries.")
-                            break 
-                        if row[4] == password: 
-                            print("Logged in.....!")
-                            curr_id = id 
+            for row in reader:
+                if not row:
+                    continue
+                if row[0] == str(id):
+                    cnt = 0
+                    while True:
+                        if cnt == 3:
+                            print("You've exceeded the number of tries.")
                             break
-                        else : 
+                        if row[4] == password:
+                            print("Logged in.....!")
+                            self.curr_id = id
+                            return
+                        else:
                             print("wrong passowrd.")
-                else: 
+                else:
                     print("User doesn't exit!..sign up please.")
-                        
+
     def sign_up(self, name, age, phone, password):
         global row_count
-        with open(file_name, 'a', newline='') as file:
+        with open(file_name, "a", newline="") as file:
             writer = csv.writer(file)
+            rows = list(writer)
+            row_count = len(rows)
             global curr_id
             id = row_count
             writer.writerow([id, name, age, phone, password])
-            curr_id = id 
+            self.curr_id = id
 
-    
     def buy():
-        pass 
+        pass
 
 
-
-cus = Customer() 
-cus.login(1, 'iislamgom3a')
+cus = Customer()
+cus.login(1, "iislamgom3a")
